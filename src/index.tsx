f1ea046d2a19898bc65ac13806c58130bd5de703
lopes-joe <slopes.joe@gmail.com>
2021-10-08 09:52:58 -0300

    Mirage instalado, feita 1ª conexão

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,19 @@
+import { createServer } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
+
+createServer({
+  routes(){
+    this.namespace = 'api'
+    this.get('/transactions', ()=>{
+      return [{
+        atribute1 : "value of atribute 1",
+        atribute2 : "value of atribute 2"
+      }]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
