bb66f584ddec81026b7e16ca214b98f824d3d945
lopes-joe <slopes.joe@gmail.com>
2021-10-08 16:54:19 -0300

    Janela modal criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+    function handleOpenNewTransactionModal(){
+        setIsNewTransactionModalOpen(true)
+    }
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModalOpen(false)
+    }
   
   return (
     <>
-      <Header/>
+      <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
+        <h1>My first modal Jeová Jiré</h1>
+      </Modal>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
       <GlobalStyle />
     </>
